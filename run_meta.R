3d138682ade6f14ea16579c363bbbadcfc8712dd
Lois Randolph <150988891+Low-is@users.noreply.github.com>
2026-04-27 16:04:44 -0500

    Update run_meta.R

--- run_meta.R
+++ run_meta.R
@@ -1,17 +1,31 @@
 library(yaml)
 library(jsonlite)
+library(COCONUT) # need to include installation of COCONUT in setup.R script
 
+# Loading config file
+message("Loading config file...")
 config <- yaml::read_yaml("Multi-Cohort_Meta-Analysis/config/config.yaml")
+message("Config file loaded!")
 
+# Loading named list of studies
+message("Loading list of studies...")
 dna_studies <- jsonlite::fromJSON(config$analysis$input$dna_gse_file)
 rna_studies <- jsonlite::fromJSON(config$analysis$input$rna_gse_file)
+message("Studies loaded!")
+
+# Loading expression matrices
+message("Loading expression matrices...")
+dna_matrices <-  readRDS("meta/matrices/dna_matrices.rds")
+rna_matrices <- readRDS("meta/matrices/rna_matrices.rds")
+message("Matrices loaded!")
 
 # Need to add code that filters matrices to match dimmensions of pData
 
 # Find common genes across all studies being used for meta-analysis
 common_genes <- find_common_genes(DNA = config$analysis$modalities$DNA,
                                   RNA = config$analysis$modalities$RNA,
-                                  list_of_dna_mtx = matrices_dna[dna_studies],
-                                  list_of_rna_mtx = matrices_RNA[rna_studies],
+                                  list_of_dna_mtx = dna_matrices[dna_studies],
+                                  list_of_rna_mtx = rna_matrices[rna_studies],
                                   use_DEG = config$analysis$use_DEG
                                  )
+

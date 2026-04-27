d435c75e333eab932662aeb9c3bedebed1518dd5
Lois Randolph <150988891+Low-is@users.noreply.github.com>
2026-04-27 15:17:22 -0500

    Update run_meta.R

--- run_meta.R
+++ run_meta.R
@@ -1,4 +1,17 @@
+library(yaml)
+library(jsonlite)
+
+config <- yaml::read_yaml("Multi-Cohort_Meta-Analysis/config/config.yaml")
+
+dna_studies <- jsonlite::fromJSON(config$analysis$input$dna_gse_file)
+rna_studies <- jsonlite::fromJSON(config$analysis$input$rna_gse_file)
+
 # Need to add code that filters matrices to match dimmensions of pData
 
 # Find common genes across all studies being used for meta-analysis
-common_genes <- find_common_genes(DNA = TRUE,
+common_genes <- find_common_genes(DNA = config$analysis$modalities$DNA,
+                                  RNA = config$analysis$modalities$RNA,
+                                  list_of_dna_mtx = matrices_dna[dna_studies],
+                                  list_of_rna_mtx = matrices_RNA[rna_studies],
+                                  use_DEG = config$analysis$use_DEG
+                                 )

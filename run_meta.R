7bad34ad8e6be04644cc6ac98ce3ec75283ab2b4
Lois Randolph <150988891+Low-is@users.noreply.github.com>
2026-04-27 15:06:02 -0500

    Create run_meta.R

---
+++ run_meta.R
@@ -0,0 +1,4 @@
+# Need to add code that filters matrices to match dimmensions of pData
+
+# Find common genes across all studies being used for meta-analysis
+common_genes <- find_common_genes(DNA = TRUE,
